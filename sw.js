7f17b6d003153e47655ee4b97571beb4bf51a702
kjjr7329-dotcom <kjjr7329@gmail.com>
2026-07-04 05:42:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,15 @@
-const CACHE = 'saja300-v2';
-const FILES = ['./', './index.html', './data.json', './manifest.json', './icon-192.png', './icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+// 한스포츠 예약 - 최소 서비스워커 (PWA 설치 요건 충족용)
+self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+self.addEventListener('activate', (e) => {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
+// 네트워크 우선 (예약 현황은 항상 최신 데이터 필요)
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      return fetch(e.request)
-        .then(res => {
-          if (res.ok && e.request.method === 'GET') {
-            const clone = res.clone();
-            caches.open(CACHE).then(c => c.put(e.request, clone));
-          }
-          return res;
-        })
-        .catch(() => cached);
-    })
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
